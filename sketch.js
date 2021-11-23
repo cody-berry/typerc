3303815232099d897e69e63f3c49dbbb9f9046f7
Cody <cody.tian@icloud.com>
2021-11-23 05:52:42 -0800

    archive 1st attempt. template files set up for 2nd

--- sketch.js
+++ sketch.js
@@ -1,22 +1,10 @@
 /*
 @author Cody
-@date 2021-11-11
+@date 2021-11-23
 
-version comments
-.   1-line passage, basic render of text
-.   start typing, getCurrentChar(), set incorrect/correct
-.   cursor
-.   highlight incorrect/correct
-.   text wrap
-.   word wrap
-    box above current word
-    WPM
-    accuracy
-    score
-    special screen
-    block on errors
+
+
  */
-
 
 let font
 let passage
@@ -26,7 +14,6 @@ let incorrectSound // audio cue for typing one char incorrectly
 
 function preload() {
     font = loadFont('data/lucida-console.ttf')
-    // font = loadFont('data/Meiryo-01.ttf')
 }
 
 
@@ -37,12 +24,6 @@ function setup() {
 
     correctSound = loadSound('data/correct.wav')
     incorrectSound = loadSound('data/incorrect.wav')
-
-    // passage = new Passage("Hello! My name is Cody Tian. Right now, I'm" +
-    //     " working on creating WPM! It's a special feature in TypingClub that" +
-    //     " uses the amount of time spent. It is also changed by a toggle" +
-    //     " button that indicates it is paused/resumed. ")
-    passage = new Passage("Today is November 16th, 2021.                ")
 }
 
 
@@ -50,52 +31,4 @@ function draw() {
     background(234, 34, 24)
 
     passage.render()
-    // console.log(passage.text)
-    // console.log(passage.correctList)
 }
-
-
-// // retype this for familiarity
-// function keyPressed() {
-//     // don't do anything if we detect SHIFT ALT CONTROL keycodes
-//     if (keyCode === SHIFT ||
-//         keyCode === ALT ||
-//         keyCode === CONTROL ||
-//         keyCode === 20) { // this is capslock
-//         return
-//     }
-//
-//     /*  if the key we just pressed === passage.getCurrentChar, play correct
-//         sound, rewind it, passage.setCorrect(). otherwise, play and rewind
-//         the incorrect sound. passage.setIncorrect().
-//      */
-//     if (passage.getCurrentChar() === key) {
-//         passage.setCorrect()
-//         correctSound.play()
-//     } else {
-//         passage.setIncorrect()
-//         incorrectSound.play()
-//     }
-// }
-
-function keyPressed() {
-    // we don't want our things to trigger if we press Shift, Alt, Control,
-    // or Capslock. The capslock keycode is 20.
-    // TODO disable windows from typing
-    if (keyCode === ALT ||
-        keyCode === SHIFT ||
-        keyCode === CONTROL ||
-        keyCode === 20) {
-        return
-    }
-
-    // let's set our correct keys to be correct!
-    if (passage.getCurrentChar() === key) {
-        passage.setCorrect()
-        correctSound.play()
-    } else {
-        passage.setIncorrect()
-        incorrectSound.play()
-    }
-}
-
